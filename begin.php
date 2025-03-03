8984b528366fbee44172ad309835630ebac3c81c
Stefan Cheplick <stefan.cheplick@gmail.com>
2025-03-03 12:54:01 -0500

    Update begin.php

--- begin.php
+++ begin.php
@@ -12,6 +12,8 @@ function trading_begin_page() {
         <html>
         <head>
 
+<meta name="google-adsense-account" content="ca-pub-3343506422217752">
+
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3343506422217752"
      crossorigin="anonymous"></script>
      
